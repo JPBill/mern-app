0d2c0d73f1c17a948419c97ed8a8d4ff59af4c21
Juan Pablo Bill <juampibill1995@gmail.com>
2024-01-23 12:14:19 -0300

    complete login page and create api route

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,6 @@
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { errorHandler } from '../utils/error.js';
 import User from '../models/userModel.js';
 
 export const register = async (req, res, next) => {
@@ -8,6 +10,25 @@ export const register = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json('Usuario creado éxitosamente');
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, 'Usuario no encontrado'));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword)
+      return next(errorHandler(401, 'Error en la contraseña o el email'));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie('access_token', token, { httpOnly: true })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
