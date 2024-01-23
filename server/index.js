6fb90832a38424d594a8f0583d17cfebace21184
Juan Pablo Bill <juampibill1995@gmail.com>
2024-01-23 11:25:02 -0300

    create register api route

--- server/index.js
+++ server/index.js
@@ -3,11 +3,14 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 import userRouter from './routes/userRoute.js';
+import authRouter from './routes/authRoute.js';
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 
 app.use('/server/user', userRouter);
+app.use('/server/auth', authRouter);
 
 mongoose
   .connect(process.env.MONGO)
