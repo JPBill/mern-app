20ba1c586b5476a1c9aaa87f812b0cdb2009f029
Juan Pablo Bill <juampibill1995@gmail.com>
2024-01-25 10:44:40 -0300

    create get user listing api route

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 import User from '../models/userModel.js';
+import Listing from '../models/listingModel.js';
 
 export const test = (req, res) => {
   res.json({
@@ -44,5 +45,18 @@ export const deleteUser = async (req, res, next) => {
     res.status(200).json('El usuario fue eliminado.');
   } catch (error) {
     next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, 'Solo puedes ver tu lista.'));
   }
 };
